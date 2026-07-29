6a60f48a296f5a0540299390eade3c921eeaec13
Asik Ali <asik14923@gmail.com>
2026-07-29 12:59:26 +0530

    A

--- lib/config.ts
+++ lib/config.ts
@@ -2,7 +2,7 @@
 export const config = {
   auth: {
     username: process.env.ADMIN_USERNAME || 'admin',
-    password: process.env.ADMIN_PASSWORD_HASH || '$2a$10$ox1/iDPv1FRYrWw3lu9fTe2NTzzLWvVCB7PSB7Orhs1OzKS/cApEW',
+    password: process.env.ADMIN_PASSWORD_HASH || '$2a$10$0M2kS.y0fXYmUAjme4vNSO2WudtAMT.xg1ItE/Z0kf.MW66XB7/tC',
   },
   session: {
     secret: process.env.SESSION_SECRET || 'a1b2c3d4e5f6789012345678901234567890abcdef1234567890abcdef123456',
